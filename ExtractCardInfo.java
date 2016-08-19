f1099f1447bc33b40229b0b3003060f09098f316
ForceRockWONG <force.rock.wong@gmail.com>
2016-08-19 09:17:24 +0800

    V3
    The Java version is much faster as it will not try to seach XPath in the
    whole file again and again.
    A Perl version is also available.
    And they both add text for Class, Rarity and CardType.

--- ExtractCardInfo.java
+++ ExtractCardInfo.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,28 +13,37 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 public class ExtractCardInfo extends JFrame {
-	private static final long serialVersionUID = 2L;
+	private static final long serialVersionUID = -7844908032131837091L;
+	private static final String VERSION_NO = "3";
 	private static final int HEIGHT_OF_WINDOW = 250;
 	private final JTextField fileField = new JTextField("");
 
-	private final JLabel resultLabel = new JLabel("You need to download the xml from https://github.com/HearthSim/hs-data/CardDefs.xml");
+	private final JLabel resultLabel = new JLabel("You need to download CardDefs.xml from https://github.com/HearthSim/hs-data");
 	private final JButton startButton = new JButton("Start");
 	private static ExtractCardInfo tm;
 
 	private static String sourceFile;
 
 	private ExtractCardInfo() {
-		super("Extract Card Information from hs-data" + " v" + serialVersionUID);
+		super("Extract Card Information from hs-data" + " v" + VERSION_NO);
 		startButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				sourceFile = fileField.getText();
@@ -54,48 +65,111 @@ public class ExtractCardInfo extends JFrame {
 				}
 			}
 
+			private String nodeToString(Node node) {
+				StringWriter sw = new StringWriter();
+				try {
+					Transformer t = TransformerFactory.newInstance().newTransformer();
+					t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+					t.transform(new DOMSource(node), new StreamResult(sw));
+				} catch (TransformerException te) {
+					System.out.println("nodeToString Transformer Exception");
+				}
+				return sw.toString();
+			}
+
+			private String addText2Class(String value) {
+				String text = "";
+
+				if (value.equals("2")) {
+					text = "Druid";
+				} else if (value.equals("3")) {
+					text = "Hunter";
+				} else if (value.equals("4")) {
+					text = "Mage";
+				} else if (value.equals("5")) {
+					text = "Paladin";
+				} else if (value.equals("6")) {
+					text = "Prist";
+				} else if (value.equals("7")) {
+					text = "Rogue";
+				} else if (value.equals("8")) {
+					text = "Shaman";
+				} else if (value.equals("9")) {
+					text = "Warlock";
+				} else if (value.equals("10")) {
+					text = "Warrior";
+				} else if (value.equals("12")) {
+					text = "Natural";
+				}
+
+				return value + text;
+			}
+
+			private String addText2Rarity(String value) {
+				String text = "";
+
+				if (value.equals("1")) {
+					text = "Common";
+				} else if (value.equals("2")) {
+					text = "Basic";
+				} else if (value.equals("3")) {
+					text = "Rare";
+				} else if (value.equals("4")) {
+					text = "Epic";
+				} else if (value.equals("5")) {
+					text = "Legindary";
+				}
+
+				return value + text;
+			}
+
+			private String addText2CardType(String value) {
+				String text = "";
+
+				if (value.equals("4")) {
+					text = "Minion";
+				} else if (value.equals("5")) {
+					text = "Spell";
+				} else if (value.equals("7")) {
+					text = "Weapon";
+				} else if (value.equals("3")) {
+					text = "Hero";
+				}
+
+				return value + text;
+			}
+
 			private void extract(PrintWriter pw) throws Exception {
 				Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File(sourceFile));
 				XPath xpath = XPathFactory.newInstance().newXPath();
-				XPathExpression expr = xpath.compile("//Entity");
+				XPathExpression expr = xpath.compile("/CardDefs/Entity");
 				NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
 				for (int i = 0; i < nodes.getLength(); i++) {
-					Node entity = nodes.item(i);
-					// CardID in <Entity>
-					String cardID = entity.getAttributes().getNamedItem("CardID").getNodeValue();
+					Element entity = (Element) nodes.item(i);
+					doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(nodeToString(entity))));
 
-					try {
-						// Collectible
-						expr = xpath.compile("//Entity[@CardID='" + cardID + "']/Tag[@name='Collectible']");
-						String cardCollectible = ((NodeList) expr.evaluate(doc, XPathConstants.NODESET)).item(0).getAttributes().getNamedItem("value").getNodeValue();
-						if (!cardCollectible.equals("1")) {
-							continue;
-						}
-
-						// CardName
-						expr = xpath.compile("//Entity[@CardID='" + cardID + "']/Tag[@name='CardName']/enUS");
-						String cardName = (String) expr.evaluate(doc, XPathConstants.STRING);
-
-						// Cost , Class , Rarity , CardSet , CardType
-						expr = xpath.compile("//Entity[@CardID='" + cardID + "']/Tag[@name='Cost']");
-						String cardCost = ((NodeList) expr.evaluate(doc, XPathConstants.NODESET)).item(0).getAttributes().getNamedItem("value").getNodeValue();
-						String cardClass = "";
-						try {
-							expr = xpath.compile("//Entity[@CardID='" + cardID + "']/Tag[@name='Class']");
-							cardClass = ((NodeList) expr.evaluate(doc, XPathConstants.NODESET)).item(0).getAttributes().getNamedItem("value").getNodeValue();
-						} catch (Exception eCardClass) {
-							cardClass = "1";
-						}
-						expr = xpath.compile("//Entity[@CardID='" + cardID + "']/Tag[@name='Rarity']");
-						String cardRarity = ((NodeList) expr.evaluate(doc, XPathConstants.NODESET)).item(0).getAttributes().getNamedItem("value").getNodeValue();
-						expr = xpath.compile("//Entity[@CardID='" + cardID + "']/Tag[@name='CardSet']");
-						String cardCardSet = ((NodeList) expr.evaluate(doc, XPathConstants.NODESET)).item(0).getAttributes().getNamedItem("value").getNodeValue();
-						expr = xpath.compile("//Entity[@CardID='" + cardID + "']/Tag[@name='CardType']");
-						String cardCardType = ((NodeList) expr.evaluate(doc, XPathConstants.NODESET)).item(0).getAttributes().getNamedItem("value").getNodeValue();
-
-						pw.println(cardCost + "\t" + cardName + "\t" + cardClass + "\t" + cardRarity + "\t" + cardCardSet + "\t" + cardID + "\t" + cardCardType);
-					} catch (Exception e) {
+					// Collectible?
+					xpath = XPathFactory.newInstance().newXPath();
+					String cardCollectible = xpath.compile("/Entity/Tag[@name='Collectible']/@value").evaluate(doc);
+					if (!cardCollectible.equals("1")) {
+						continue;
 					}
+					// Hero?
+					xpath = XPathFactory.newInstance().newXPath();
+					String cardCardType = xpath.compile("/Entity/Tag[@name='CardType']/@value").evaluate(doc);
+					if (cardCardType.equals("3")) {
+						continue;
+					}
+
+					String cardCost = xpath.compile("/Entity/Tag[@name='Cost']/@value").evaluate(doc);
+					String cardName = xpath.compile("/Entity/Tag[@name='CardName']/enUS").evaluate(doc);
+					String cardClass = addText2Class(xpath.compile("/Entity/Tag[@name='Class']/@value").evaluate(doc));
+					String cardRarity = addText2Rarity(xpath.compile("/Entity/Tag[@name='Rarity']/@value").evaluate(doc));
+					String cardCardSet = xpath.compile("/Entity/Tag[@name='CardSet']/@value").evaluate(doc);
+					String cardID = entity.getAttribute("CardID");
+					cardCardType = addText2CardType(cardCardType);
+
+					pw.println(cardCost + "\t" + cardName + "\t" + cardClass + "\t" + cardRarity + "\t" + cardCardSet + "\t" + cardID + "\t" + cardCardType);
 				}
 			}
 		});
---
+++ ExtractCardInfo.pl
@@ -0,0 +1,128 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use XML::XPath;
+use XML::XPath::XMLParser;
+
+
+#1. Get <Entity>
+#2. Parse <Entity>
+#	2.1. Collectible?		<Tag enumID="321" name="Collectible" type="Bool" value="1">
+#		2.1.1. Next unless value is true
+#   2.2. Hero?				<Tag enumID="202" name="CardType" type="CardType" value="3"/>
+#       2.2.1. Next if value == 3
+#   2.2. We need to get:
+#		2.2.1. Cost			<Tag enumID="48" name="Cost" type="Int" value="5"/>
+#		2.2.2. Name			<Tag enumID="185" name="CardName" type="LocString"><enUS>Flame Lance</enUS></Tag>
+#		2.2.3. Class		<Tag enumID="199" name="Class" type="Class" value="4"/>
+#		2.2.4. Rarity		<Tag enumID="203" name="Rarity" type="Rarity" value="1"/>
+#		2.2.5. CardSet		<Tag enumID="183" name="CardSet" type="CardSet" value="15"/>
+#		2.2.6. CardID		<Entity CardID="AT_001" version="2">
+#		2.2.7. CardType		<Tag enumID="202" name="CardType" type="CardType" value="5"/>
+#3. Save to CardDB.txt
+
+#1. Get <Entity>
+my $entities_ref = getEntities();
+#2. Parse <Entity>
+$entities_ref = keepCollectible($entities_ref);
+#3. Save to CardDB.txt
+saveToTxt($entities_ref);
+print "Done\n";
+
+sub saveToTxt {
+	#title of each column
+	open FH, "> CardDB.txt" or die $!;
+	print FH "Cost\tName\tClass\tRarity\tCardSet\tCardID\tCardType\n";
+	# 2.1.6. save the temp file
+	my $cards_ref = shift;
+	for my $card_ref (@{$cards_ref}) {
+		print FH $card_ref->{"Cost"},		"\t";
+		print FH $card_ref->{"CardName"},	"\t";
+		print FH $card_ref->{"Class"},		"\t";
+		print FH $card_ref->{"Rarity"},		"\t";
+		print FH $card_ref->{"CardSet"},	"\t";
+		print FH $card_ref->{"CardID"},		"\t";
+		print FH $card_ref->{"CardType"},	"\n";
+	}
+	close FH;
+}
+
+sub keepCollectible {
+	my $entities_ref = shift;
+	my @entities;
+	for my $anEntity (@{$entities_ref}) {
+		my $xp = XML::XPath->new(xml => $anEntity);
+		#2.1. Collectible?
+		next unless $xp->findvalue('/Entity/Tag[@name="Collectible"]/@value');
+		#2.2. Hero?
+		my $CardType = $xp->findvalue('/Entity/Tag[@name="CardType"]/@value');
+		next if $CardType == 3;
+		#2.3. We need to get:
+		my %card = (
+			Cost		=> $xp->findvalue('/Entity/Tag[@name="Cost"]/@value'),
+			CardName	=> $xp->findvalue('/Entity/Tag[@name="CardName"]/enUS'),
+			Class		=> addText2Class($xp->findvalue('/Entity/Tag[@name="Class"]/@value')),
+			Rarity		=> addText2Rarity($xp->findvalue('/Entity/Tag[@name="Rarity"]/@value')),
+			CardSet		=> $xp->findvalue('/Entity/Tag[@name="CardSet"]/@value'),
+			CardID		=> $xp->findvalue('/Entity/@CardID'),
+			CardType	=> addText2CardType($CardType)
+		);
+		push @entities, \%card;
+	}
+	return \@entities;
+}
+
+sub addText2Class {
+	my $value = shift;
+	my $text;
+	
+	if		($value == 2) { $text = "Druid";}
+	elsif	($value == 3) { $text = "Hunter";}
+	elsif	($value == 4) { $text = "Mage";}
+	elsif	($value == 5) { $text = "Paladin";}
+	elsif	($value == 6) { $text = "Prist";}
+	elsif	($value == 7) { $text = "Rogue";}
+	elsif	($value == 8) { $text = "Shaman";}
+	elsif	($value == 9) { $text = "Warlock";}
+	elsif	($value == 10){ $text = "Warrior";}
+	elsif	($value == 12){ $text = "Natural";}
+	
+	return "$value$text";
+}
+
+sub addText2Rarity {
+	my $value = shift;
+	my $text;
+	
+	if		($value == 1) { $text = "Common";}
+	elsif	($value == 2) { $text = "Basic";}
+	elsif	($value == 3) { $text = "Rare";}
+	elsif	($value == 4) { $text = "Epic";}
+	elsif	($value == 5) { $text = "Legindary";}
+	
+	return "$value$text";
+}
+
+sub addText2CardType {
+	my $value = shift;
+	my $text;
+	
+	if		($value == 4) { $text = "Minion";}
+	elsif	($value == 5) { $text = "Spell";}
+	elsif	($value == 7) { $text = "Weapon";}
+	elsif	($value == 3) { $text = "Hero";}
+	
+	return "$value$text";
+}
+
+sub getEntities {
+	my @entities;
+	my $xp = XML::XPath->new(filename => $ARGV[0]);
+	my $nodeset = $xp->find("/CardDefs/Entity");
+ 	for my $nodes ($nodeset->get_nodelist) {
+		my $node = XML::XPath::XMLParser::as_string($nodes);
+		push @entities, $node;
+	}
+	return \@entities;
+}
